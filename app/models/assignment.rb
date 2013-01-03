4aba57b2604d1dee2be38c9bcd79300139540635
Ken Toole <kdt33@cornell.edu>
2013-01-02 19:54:32 -0800

    fix order on belongs_to problem

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,5 @@
 class Assignment < ActiveRecord::Base
-  belongs_to :user, :foreign_key => "user_id", :order => "name ASC"
+  belongs_to :user, :foreign_key => "user_id"
   belongs_to :project, :foreign_key => "project_id"
   belongs_to :set_period, :foreign_key => "set_period_id"
   attr_accessible :effort, :set_period, :set_period_id, :is_fixed, :project_id, :user_id, :user, :project

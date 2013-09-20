b2f0148e03de4bf3eb80d19c5c9b74c602113adf
Ken Toole <kdt33@cornell.edu>
2013-09-20 13:10:18 -0700

    fix is manager index

--- db/migrate/20130920194046_add_indexes.rb
+++ db/migrate/20130920194046_add_indexes.rb
@@ -1,7 +1,7 @@
 class AddIndexes < ActiveRecord::Migration
   def up
   	add_index :users, :manager_id
-  	add_index :users, :is_manager
+  	add_index :users, :ismanager
   	add_index :projects, :category
   	add_index :assignments, :user_id
   	add_index :assignments, :project_id
@@ -10,7 +10,7 @@ class AddIndexes < ActiveRecord::Migration
 
   def down
   	remove_index :users, :manager_id
-  	remove_index :users, :is_manager
+  	remove_index :users, :ismanager
   	remove_index :projects, :category
 	remove_index :assignments, :user_id
   	remove_index :assignments, :project_id

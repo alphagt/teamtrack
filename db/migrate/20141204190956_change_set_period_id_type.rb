2585641abeff4c834671fcd1ec777031734143a9
Ken Toole <kdt33@cornell.edu>
2014-12-06 10:07:09 -0800

    avoic bad fkey in assignments

--- db/migrate/20141204190956_change_set_period_id_type.rb
+++ db/migrate/20141204190956_change_set_period_id_type.rb
@@ -6,11 +6,13 @@ class ChangeSetPeriodIdType < ActiveRecord::Migration
   	Assignment.all.each do |a|
 		if a.set_period_id < 2000 then
 			@oldPeriod = SetPeriod.find_by_id(a.set_period_id.to_i)
-			@newPeriod = @oldPeriod.fiscal_year.to_f + (@oldPeriod.week_number.to_f / 100)
-			a.set_period_id = @newPeriod
-			puts "Updating Assignment ..."
-			puts a.id
-			a.save
+			if @oldPeriod != nil then
+				@newPeriod = @oldPeriod.fiscal_year.to_f + (@oldPeriod.week_number.to_f / 100)
+				a.set_period_id = @newPeriod
+				puts "Updating Assignment ..."
+				puts a.id
+				a.save
+			end
 		end
   	end
   end

e2a4be11e076b5c888f1457015c9ef8fc740ca06
Ken Toole <kdt33@cornell.edu>
2014-12-10 14:00:50 -0800

    home controller filter of users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-  	@users = User.all
+  	@exId = User.find_by_name("ExEmployeeMgr").id
+  	@users = User.where('manager_id != ?', @exId).order("manager_id,name")
   	redirect_to team_user_path(:id => current_user.id) if current_user and current_user.verified
   end
 end

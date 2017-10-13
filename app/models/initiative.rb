130249055fb352749dba373b2a498d74d9d7b99c
Ken Toole <kdt33@cornell.edu>
2017-10-13 13:11:01 -0700

    add ytd effort to initiatives index view

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -1,6 +1,15 @@
 class Initiative < ActiveRecord::Base
 has_many :projects
-
+attr_accessible :total_effort_weeks
+  	
 scope :active, -> {where('active = true')}
 scope :current_year, -> {where('fiscal = 2017')}
+
+	def total_effort_weeks
+		@ytd_weeks = 0
+		self.projects.each do |proj|
+			@ytd_weeks += proj.ytd_allocation
+		end
+		@ytd_weeks.round(1)
+	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -23,4 +23,35 @@ class Project < ActiveRecord::Base
 		end
 		fixed_resource_budget > tEffort
 	end
+	
+	def ytd_allocation
+		@ytd = 0
+		@cperiod = 0.0
+		#calc current period code
+		@cweek_number = 0.0
+		@fyear = Date.today.year 
+		if Date.today.mon == 12 then
+			@fyear = @fyear + 1
+			@cfy_offset = 4 #SetPeriod.where(:fiscal_year => @fyear).first!.cweek_offset
+			@cweek_number = Date.today.cweek + @cfy_offset - 52
+		else
+			@cfy_offset = 4 #SetPeriod.where(:fiscal_year => @fyear).first!.cweek_offset
+			@cweek_number = Date.today.cweek + @cfy_offset
+		end
+		#puts 'CURRENT CWEEK Number: '
+		#puts  @cweek_number
+		@cperiod = @fyear + @cweek_number.fdiv(100).round(3)
+		#end cperiod
+		@pFy = @cperiod.to_i
+		@fWeek = ((@cperiod - @pFy) * 100).round
+		#puts "fweek = " + @fWeek.to_s
+		for iWeek in 1..@fWeek
+			@cP = @pFy.to_f + (iWeek.to_f / 100)
+			assignments.where(:set_period_id => @cP.round(2)).each do |asn|
+				puts asn.effort.to_s
+				@ytd += asn.effort
+			end
+		end
+		@ytd.to_d.round(2)
+	end
 end

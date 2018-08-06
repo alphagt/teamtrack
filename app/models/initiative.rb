4fb32f92d7278eedeed1f143a7bda238feac9e4d
Ken Toole <kdt33@cornell.edu>
2018-08-06 14:43:08 -0700

    handle special case intiatives with no fy

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -6,8 +6,8 @@ scope :for_year, -> (y){where("fiscal = ? or name IN('Overhead','Basics')", y)}
 
 	def total_effort_weeks(cWeek)
 		@ytd_weeks = 0
-		fy = self.fiscal
-		self.projects.each do |proj|
+		fy = self.cfiscal
+		self.projects.for_year(fy).each do |proj|
 			@ytd_weeks += proj.ytd_allocation(fy, cWeek)
 		end
 		@ytd_weeks.round(1)
@@ -15,10 +15,19 @@ scope :for_year, -> (y){where("fiscal = ? or name IN('Overhead','Basics')", y)}
 	
 	def current_effort_weeks(pid)
 		@c_weeks = 0
+		fy = self.cfiscal
 		puts "current_effort_weeks for initiative for week: ?", pid.to_s
-		self.projects.each do |proj|
+		self.projects.for_year(fy).each do |proj|
 			@c_weeks += proj.assignments.where("set_period_id = ?", pid).sum("effort")
 		end
 		@c_weeks.round(1)
+	end
+	
+	def cfiscal
+		if self.fiscal.nil? then
+			Date.today.year
+		else
+			self.fiscal
+		end
 	end
 end

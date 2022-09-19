cd8efe9bc8c11fb711dedea8249009ede8512f94
Ken Toole <kdt33@cornell.edu>
2022-09-19 15:11:47 -0700

    enable impersonate users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,15 @@ class HomeController < ApplicationController
   	if aid > 0
 		@exId = User.for_account(aid).find_by_name("ExEmployeeMgr").id
 		@users = User.for_account(aid).where('manager_id != ?', @exId).order("manager_id,name")
-		if !current_user.ismanager then
+		if !current_user.ismanager && current_user.impersonate_manager.nil? then
 			redirect_to projects_path(:org => "0") if current_user and current_user.verified
 		else
-			redirect_to team_user_path(:id => current_user.id) if current_user and current_user.verified
+			if !current_user.ismanager then
+				mid = current_user.impersonate_manager
+			else
+				mid = current_user.id
+			end
+			redirect_to team_user_path(:id => mid) if current_user and current_user.verified
 		end
 	end
 

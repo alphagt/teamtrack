649180f2bba01a54769d2dd13a80705f670c8257
Ken Toole <kdt33@cornell.edu>
2023-01-13 15:28:43 -0800

    project view sort by current alloc prototype

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -326,6 +326,9 @@ class ProjectsController < ApplicationController
 	puts "user scoped project list:"
 	puts @projects.count
 	
+	prj = @projects.map{ |p| [view_context.current_allocation(p,1),p] }.to_h
+	@projects = prj.sort_by {|alloc,p| alloc}.reverse
+	
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @projects }

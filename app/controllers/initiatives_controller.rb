788733b2a8ea7cea67f2e1e378012459a71a3b8a
Ken Toole <kdt33@cornell.edu>
2024-05-24 08:51:38 -0700

    fix format string errors

--- app/controllers/initiatives_controller.rb
+++ app/controllers/initiatives_controller.rb
@@ -98,7 +98,7 @@ class InitiativesController < ApplicationController
 	puts @cdata
 	
 #Legacy separate array impl for G Charts
-	@clabels = @cdata.to_h.keys.map{|e| "week " + format('%02d', view_context.week_from_period(e)}
+	@clabels = @cdata.to_h.keys.map{|e| "week " + format('%02d', view_context.week_from_period(e))}
 	@clabels.sort!
 	@cvalues = @cdata.to_h.values
 	puts 'Gcharts Arrays:'
